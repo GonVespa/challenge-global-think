82c2b40f7beca5249d068ea806b83719f93784cb
EXTRADOS-LP-12\Gonzalo Vespasiani <gonzalo.vespasiani@extrados.ar>
2023-11-27 19:42:42 -0300

    Integracion de Jest para testig Unitario

--- src/controllers/weatherController.ts
+++ src/controllers/weatherController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request, Response } from 'express'
 import { getWeatherData } from '../services/weatherService'
 
 export async function getWeather(req: Request, res: Response): Promise<void> {
@@ -6,21 +6,21 @@ export async function getWeather(req: Request, res: Response): Promise<void> {
 
   try {
     if (!cityName) {
-      throw new Error('El parámetro cityName es obligatorio.');
+      throw new Error('El parámetro cityName es obligatorio.')
     }
     const weatherData = await getWeatherData(cityName)
     res.json(weatherData)
   } catch (error) {
-    console.error(error);
+    console.error(error)
 
     if (error instanceof Error) {
       if (error.message === 'El parámetro cityName es obligatorio.') {
-        res.status(400).json({ error: error.message });
+        res.status(400).json({ error: error.message })
       } else {
-        res.status(500).json({ error: 'Error al obtener los datos del clima.' });
+        res.status(500).json({ error: 'Error al obtener los datos del clima.' })
       }
     } else {
-      res.status(500).json({ error: 'Error interno del servidor.' });
+      res.status(500).json({ error: 'Error interno del servidor.' })
     }
   }
 }
